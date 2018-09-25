45daae327eef4064dc9cbfc9ad8b84d7d658948e
ksebesta <k_sebesta@yahoo.com>
2018-09-25 11:59:09 -0500

    Add files via upload

---
+++ Project2_55000_downsampling_10epochs.py
@@ -0,0 +1,322 @@
+import pandas as pd
+import numpy as np
+import os
+import itertools
+import sklearn
+import matplotlib.pylab as plt
+import cv2
+import seaborn as sns
+from glob import glob
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import Callback
+from keras.layers import Dense, Dropout, Flatten
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, BatchNormalization
+from imblearn.under_sampling import RandomUnderSampler
+
+PATH = os.path.abspath(os.path.join('..', 'Project 2'))
+labels = pd.read_csv('..//Project 2/sample/Data_Entry_2017.csv')
+
+SOURCE_IMAGES = os.path.join(PATH, "sample", "images")
+images = glob(os.path.join(SOURCE_IMAGES, "*.png"))
+
+labels = labels[['Image Index','Finding Labels','Follow-up #','Patient ID','Patient Age','Patient Gender']]
+
+#Preprocess Data
+def proc_images():
+    """
+    Returns two arrays:
+        x is an array of resized images
+        y is an array of labels
+    """
+    NoFinding = "No Finding" #0
+    Consolidation="Consolidation" #1
+    Infiltration="Infiltration" #2
+    Pneumothorax="Pneumothorax" #3
+    Edema="Edema" # 7
+    Emphysema="Emphysema" #7
+    Fibrosis="Fibrosis" #7
+    Effusion="Effusion" #4
+    Pneumonia="Pneumonia" #7
+    Pleural_Thickening="Pleural_Thickening" #7
+    Cardiomegaly="Cardiomegaly" #7
+    NoduleMass="Nodule" #5
+    Hernia="Hernia" #7
+    Atelectasis="Atelectasis"  #6
+    RareClass = ["Edema", "Emphysema", "Fibrosis", "Pneumonia", "Pleural_Thickening", "Cardiomegaly","Hernia"]
+    x = [] # images as arrays
+    y = [] # labels Infiltration or Not_infiltration
+    WIDTH = 64
+    HEIGHT = 64
+    for img in images:
+        base = os.path.basename(img)
+        finding = labels["Finding Labels"][labels["Image Index"] == base].values[0]
+        # Read and resize image
+        full_size_image = cv2.imread(img)
+        x.append(cv2.resize(full_size_image, (WIDTH,HEIGHT), interpolation=cv2.INTER_CUBIC))
+        # Labels
+        if NoFinding in finding:
+            finding = 0
+            y.append(finding)
+        elif Consolidation in finding:
+            finding = 1
+            y.append(finding)
+        elif Infiltration in finding:
+            finding = 2
+            y.append(finding)
+        elif Pneumothorax in finding:
+            finding = 3
+            y.append(finding)
+        elif Edema in finding:
+            finding = 7
+            y.append(finding)
+        elif Emphysema in finding:
+            finding = 7
+            y.append(finding)
+        elif Fibrosis in finding:
+            finding = 7
+            y.append(finding)
+        elif Effusion in finding:
+            finding = 4
+            y.append(finding)
+        elif Pneumonia in finding:
+            finding = 7
+            y.append(finding)
+        elif Pleural_Thickening in finding:
+            finding = 7
+            y.append(finding)
+        elif Cardiomegaly in finding:
+            finding = 7
+            y.append(finding)
+        elif NoduleMass in finding:
+            finding = 5
+            y.append(finding)
+        elif Hernia in finding:
+            finding = 7
+            y.append(finding)
+        elif Atelectasis in finding:
+            finding = 6
+            y.append(finding)
+        else:
+            finding = 7
+            y.append(finding)
+    return x,y
+
+#part 2
+X,y = proc_images()
+df = pd.DataFrame()
+df["images"]=X
+df["labels"]=y
+
+#Describe new numpy arrays
+dict_characters = {0: 'No Finding', 1: 'Consolidation', 2: 'Infiltration',
+        3: 'Pneumothorax', 4:'Effusion', 5: 'Nodule Mass', 6: 'Atelectasis', 7: "Other Rare Classes"}
+
+print(df.head(10))
+print("")
+print(dict_characters)
+
+#show unbalance
+lab = df['labels']
+dist = lab.value_counts()
+sns.countplot(lab)
+print(dict_characters)
+
+X=np.array(X)
+X=X/255.0
+
+#test&train
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
+print("Training Data Shape:", X_train.shape)
+print("Testing Data Shape:", X_test.shape)
+print("Training Data Shape:", len(X_train), X_train[0].shape)
+print("Testing Data Shape:", len(X_test), X_test[0].shape)
+
+# try to use a CNN to predict each ailment based off of the X-Ray image.
+# Encode labels to hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
+Y_trainHot = to_categorical(Y_train, num_classes = 8)
+Y_testHot = to_categorical(Y_test, num_classes = 8)
+
+# In order to avoid having a biased model because of skewed class sizes,
+# We need to modify the class_weights parameter in order to give more weight to the rare classes.
+# In this case the class_weights parameter will eventually be passed to the model.fit function.
+
+from sklearn.utils import class_weight
+class_weight = class_weight.compute_class_weight('balanced', np.unique(y), y)
+print(class_weight)
+
+# Helper Functions  Learning Curves and Confusion Matrix
+
+
+class MetricsCheckpoint(Callback):
+    """Callback that saves metrics after each epoch"""
+    def __init__(self, savepath):
+        super(MetricsCheckpoint, self).__init__()
+        self.savepath = savepath
+        self.history = {}
+    def on_epoch_end(self, epoch, logs=None):
+        for k, v in logs.items():
+            self.history.setdefault(k, []).append(v)
+        np.save(self.savepath, self.history)
+
+def plotKerasLearningCurve():
+    plt.figure(figsize=(10,5))
+    metrics = np.load('logs.npy')[()]
+    filt = ['acc'] # try to add 'loss' to see the loss learning curve
+    for k in filter(lambda x : np.any([kk in x for kk in filt]), metrics.keys()):
+        l = np.array(metrics[k])
+        plt.plot(l, c= 'r' if 'val' not in k else 'b', label='val' if 'val' in k else 'train')
+        x = np.argmin(l) if 'loss' in k else np.argmax(l)
+        y = l[x]
+        plt.scatter(x,y, lw=0, alpha=0.25, s=100, c='r' if 'val' not in k else 'b')
+        plt.text(x, y, '{} = {:.4f}'.format(x,y), size='15', color= 'r' if 'val' not in k else 'b')
+    plt.legend(loc=4)
+    plt.axis([0, None, None, None]);
+    plt.grid()
+    plt.xlabel('Number of epochs')
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure(figsize = (5,5))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+#CNN
+def runCNNconfusion(a,b,c,d):
+    # In -> [[Conv2D->relu]*2 -> MaxPool2D -> Dropout]*2 -> Flatten -> Dense -> Dropout -> Out
+    batch_size = 128
+    num_classes = 8
+    epochs = 10
+    img_rows, img_cols = X_train.shape[1],X_train.shape[2]
+    input_shape = (img_rows, img_cols, 3)
+    model = Sequential()
+    model.add(Conv2D(filters = 32, kernel_size = (3,3),padding = 'Same',
+                     activation ='relu', input_shape = input_shape))
+    model.add(Conv2D(filters = 32, kernel_size = (3,3),padding = 'Same',
+                     activation ='relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+    model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',
+                     activation ='relu'))
+    model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',
+                     activation ='relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+    model.add(Conv2D(filters = 86, kernel_size = (3,3),padding = 'Same',
+                     activation ='relu'))
+    model.add(Conv2D(filters = 86, kernel_size = (3,3),padding = 'Same',
+                     activation ='relu'))
+    model.add(MaxPool2D(pool_size=(2,2)))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    #model.add(Dense(1024, activation = "relu"))
+    #model.add(Dropout(0.5))
+    model.add(Dense(512, activation = "relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation = "softmax"))
+    # Define the optimizer
+    optimizer = RMSprop(lr=0.001, decay=1e-6)
+    model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+#         rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+#         width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+#         height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+#         horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+    datagen.fit(a)
+    model.fit_generator(datagen.flow(a,b, batch_size=32),
+                        steps_per_epoch=len(a) / 32, epochs=epochs, class_weight = class_weight,  validation_data = [c, d],callbacks = [MetricsCheckpoint('logs')])
+    score = model.evaluate(c,d, verbose=0)
+    print('\nKeras CNN #2B - accuracy:', score[1],'\n')
+    Y_pred = model.predict(c)
+    print('\n', sklearn.metrics.classification_report(np.where(d > 0)[1], np.argmax(Y_pred, axis=1), target_names=list(dict_characters.values())), sep='')
+    Y_pred_classes = np.argmax(Y_pred,axis = 1)
+    Y_true = np.argmax(d,axis = 1)
+    confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)
+    plot_confusion_matrix(confusion_mtx, classes = list(dict_characters.values()))
+
+#Make Data 1D for compatability upsampling methods
+X_trainShape = X_train.shape[1]*X_train.shape[2]*X_train.shape[3]
+X_testShape = X_test.shape[1]*X_test.shape[2]*X_test.shape[3]
+X_trainFlat = X_train.reshape(X_train.shape[0], X_trainShape)
+X_testFlat = X_test.reshape(X_test.shape[0], X_testShape)
+print("X_train Shape: ",X_train.shape)
+print("X_test Shape: ",X_test.shape)
+print("X_trainFlat Shape: ",X_trainFlat.shape)
+print("X_testFlat Shape: ",X_testFlat.shape)
+
+#downsize
+ros = RandomUnderSampler(ratio='auto')
+X_trainRos, Y_trainRos = ros.fit_sample(X_trainFlat, Y_train)
+X_testRos, Y_testRos = ros.fit_sample(X_testFlat, Y_test)
+
+# Encode labels to hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
+Y_trainRosHot = to_categorical(Y_trainRos, num_classes = 8)
+Y_testRosHot = to_categorical(Y_testRos, num_classes = 8)
+print("X_train: ", X_train.shape)
+print("X_trainFlat: ", X_trainFlat.shape)
+print("X_trainRos Shape: ",X_trainRos.shape)
+print("X_testRos Shape: ",X_testRos.shape)
+
+for i in range(len(X_trainRos)):
+    height, width, channels = 64,64,3
+    X_trainRosReshaped = X_trainRos.reshape(len(X_trainRos),height,width,channels)
+print("X_trainRos Shape: ",X_trainRos.shape)
+print("X_trainRosReshaped Shape: ",X_trainRosReshaped.shape)
+
+for i in range(len(X_testRos)):
+    height, width, channels = 64,64,3
+    X_testRosReshaped = X_testRos.reshape(len(X_testRos),height,width,channels)
+print("X_testRos Shape: ",X_testRos.shape)
+print("X_testRosReshaped Shape: ",X_testRosReshaped.shape)
+
+dfRos = pd.DataFrame()
+dfRos["labels"]=Y_trainRos
+labRos = dfRos['labels']
+distRos = lab.value_counts()
+sns.countplot(labRos)
+print(dict_characters)
+
+from sklearn.utils import class_weight
+class_weight = class_weight.compute_class_weight('balanced', np.unique(y), y)
+print("Old Class Weights: ",class_weight)
+from sklearn.utils import class_weight
+class_weight = class_weight.compute_class_weight('balanced', np.unique(Y_trainRos), Y_trainRos)
+print("New Class Weights: ",class_weight)
+
+runCNNconfusion(X_trainRosReshaped, Y_trainRosHot, X_testRosReshaped, Y_testRosHot)
+plotKerasLearningCurve()
